78ff32caf1d47638f2de7ae4f13fc635532914ff
Ganesh Sivadasan <ganeshsivadasan18@gmail.com>
2026-03-23 16:57:06 -0400

    feat: add finale timeline start

--- Assets/Scenes/Scripts/FinalSceneManager.cs
+++ Assets/Scenes/Scripts/FinalSceneManager.cs
@@ -1,16 +1,21 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class FinalSceneManager : MonoBehaviour
 {
+    [SerializeField] PlayableDirector finaleTimeline;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         GameObject.FindGameObjectWithTag("Player").SetActive(false);
+        MusicManager.Instance.StopMusic(0);
+        StartCoroutine(FinaleStart());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator FinaleStart()
     {
-        
+        yield return new WaitForSeconds(5);
+        finaleTimeline.Play();
     }
 }
